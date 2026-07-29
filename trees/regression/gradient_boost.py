3e1739cffef9c0c04e7ad49e05c7fa2611364cf0
vDenight <84413087+vDenight@users.noreply.github.com>
2026-07-29 13:25:32 +0200

    small fix to gboost

--- trees/regression/gradient_boost.py
+++ trees/regression/gradient_boost.py
@@ -8,12 +8,13 @@ class GradientBoost:
     def __init__(
         self,
         n_estimators: int,
+        learning_rate: float,
         max_depth: int,
         min_samples_split: int,
         min_samples_leaf: int,
     ) -> None:
         self.n_estimators = n_estimators
-        self.learning_rate: float = 0.1
+        self.learning_rate: float = learning_rate
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
         self.min_samples_leaf = min_samples_leaf
